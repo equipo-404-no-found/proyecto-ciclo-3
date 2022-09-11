873b3a18df89ff9fba392a9b14ef8e1ead212536
Carlosjavierfe <carlosjaviermendezgutierrez@gmail.com>
2022-09-10 23:35:34 -0500

    .

--- libreria/src/main/java/com/crud/libreria/repository/EmpleadoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crud.libreria.repository;
-
-import com.crud.libreria.model.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
-}
--- libreria/src/main/java/com/crud/libreria/repository/MovDineroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crud.libreria.repository;
-
-import com.crud.libreria.model.MovDinero;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovDineroRepository extends JpaRepository<MovDinero, Long> {
-}
