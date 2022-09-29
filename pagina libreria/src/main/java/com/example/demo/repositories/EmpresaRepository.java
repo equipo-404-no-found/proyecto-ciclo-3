bd206e76614cda0306e739db379ecde31e0e7d4d
jheinner <jheinnerc12@gmail.com>
2022-09-28 22:32:20 -0500

    pagina web de libreria

---
+++ pagina libreria/src/main/java/com/example/demo/repositories/EmpresaRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository //Anotacion que le dice a Spring que esta clase es un repositorio
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
+}
